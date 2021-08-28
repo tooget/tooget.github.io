8550d7ee25503385a1acc4696e565f6eaf0f6e28
SUNGHYUN KIM <tooget@gmail.com>
2021-08-28 12:06:11 +0900

    feat: [#3] Add input form for customer tickets

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,22 +1,7 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="(issueInfo, index) in listedJsonIssuesInfo" :key="index">
-        <a :href="'issueInfo.url'" target="_blank" rel="noopener noreferrer">
-          {{ issueInfo.title }}
-        </a>
-      </li>
-    </ul>
-  </div>
+  <v-container>
+    <CustomerInputForm />
+  </v-container>
 </template>
 
-<script>
-export default {
-  async asyncData({ $axios }) {
-    const listedJsonIssuesInfo = await $axios.$get('https://api.github.com/repos/tooget/tooget.github.io/issues?state=all')
-    return {
-      listedJsonIssuesInfo,
-    }
-  },
-}
-</script>
+
