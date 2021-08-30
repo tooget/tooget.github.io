3f139831d0bf872909f7f1a154037d0f5408e193
SUNGHYUN KIM <tooget@gmail.com>
2021-08-31 00:54:47 +0900

    refactor: [#3] remove unused components and files

--- pages/askmeanything.vue
+++ pages/askmeanything.vue
@@ -1,22 +1,36 @@
 <template>
   <v-container>
-    <AskingInputForm />
-    <AskingIssuesList
-        :issuesListResultFromAskmeanythingPage="issuesListResult"
-    />
+    <v-row justify="space-around">
+      <v-card width="400">
+        <v-container justify="center">
+          <AskingInputForm />
+        </v-container>
+        <v-container>
+          <AskingIssuesList
+            :issuesListResultFromAskmeanythingPage="issuesListResult"
+          />
+        </v-container>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/"> SUNGHYUN KIM </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   async asyncData({ $axios }) {
-    const issuesListResult = await $axios.$get('https://api.github.com/repos/tooget/tooget.github.io/issues?state=all')
+    const issuesListResult = await $axios.$get(
+      'https://api.github.com/repos/tooget/tooget.github.io/issues?state=all'
+    )
     return { issuesListResult }
   },
   data() {
     return {
-      issuesListResult: []
+      issuesListResult: [],
     }
-  }
+  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,12 @@
         <v-container class="d-flex flex-row">
           <MyLinkedinProfile />
         </v-container>
-        <MyTimelineProfile />
+          <MyTimelineProfile />
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/askmeanything"> Continue </v-btn>
+          <v-btn color="primary" nuxt to="/askmeanything">
+            Ask me anything!
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-row>
